1c53115723b73f4e167938459a0b125dec10e5c8
Leo Sandoval <lsandova@redhat.com>
2024-02-21 18:59:21 -0600

    minor changes on 1.-Procesos

--- 1.-Procesos/mem_compartida_ejemplo.c
+++ 1.-Procesos/mem_compartida_ejemplo.c
@@ -7,35 +7,32 @@ int *g;
 
 int main()
 {
-	int pid;
-	int i;
-	
-	int shmid = shmget(0x1234,sizeof(int),0666|IPC_CREAT);
-	g = shmat(shmid,NULL,0);
-	
-	*g=0;
-		
-	pid=fork();
-	// Todas las variables locales y globales se 
-	// duplican
-	
-	if(pid==0)	// El hijo
-	{
-		for(i=0;i<5;i++)
-		{
-			(*g)++;
-			printf("En el hijo g=%d\n",*g);
-			sleep(1);
-		}
-	}
-	else
-	{
-		for(i=0;i<10;i++)
-		{
-			printf("En el padre g=%d\n",*g);
-			sleep(1);
-		}
-		shmdt(shmid);
-	}
-	
+  int pid;
+  int i;
+
+  int shmid = shmget(0x1234,sizeof(int),0666|IPC_CREAT);
+  g = shmat(shmid,NULL,0);
+
+  *g=0;
+
+  pid=fork();
+  // Todas las variables locales y globales se duplican
+  if(pid==0)	// El hijo
+    {
+      for(i=0;i<5;i++)
+        {
+          (*g)++;
+          printf("En el hijo g=%d\n",*g);
+          sleep(1);
+        }
+    }
+  else
+    {
+      for(i=0;i<10;i++)
+        {
+          printf("En el padre g=%d\n",*g);
+          sleep(1);
+        }
+      shmdt(shmid);
+    }
 }
